a6c54e23a68004c12ee35c24c1aaef43531b9776
leo <nascimentoleo899@gmail.com>
2024-03-29 16:03:45 -0300

    Add email service dependency to emprestarLivroUseCase

--- src/application/emprestar-livro.usecase.js
+++ src/application/emprestar-livro.usecase.js
@@ -1,7 +1,7 @@
 const { AppError, Either } = require("../shared/errors");
 
-module.exports = function emprestarLivroUseCase({ emprestimoRepository }) {
-  if(!emprestimoRepository) throw new AppError(AppError.dependencies);
+module.exports = function emprestarLivroUseCase({ emprestimoRepository, emailService }) {
+  if(!emprestimoRepository || !emailService) throw new AppError(AppError.dependencies);
   return async function({livro_id, usuario_id, data_saida, data_retorno}) {
     const isValidFields = livro_id && usuario_id && data_saida && data_retorno;
     if(!isValidFields) return Either.left(AppError.fieldsObligatory);
@@ -10,12 +10,22 @@ module.exports = function emprestarLivroUseCase({ emprestimoRepository }) {
     if(existeLivroISBNPendenteUsuario) return Either.left(AppError.livroJaEmprestado);
     const quantidadeLivrosEmprestadoPorUsuario = await emprestimoRepository.quantidadeLivrosEmprestadoPorUsuario(usuario_id);
     if(quantidadeLivrosEmprestadoPorUsuario >= 3) return Either.left(AppError.usuarioComLimiteEmprestimos);
-    await emprestimoRepository.emprestarLivro({
+    const id = await emprestimoRepository.emprestarLivro({
       livro_id,
       usuario_id,
       data_saida,
       data_retorno,
     });
+    
+    const {usuario, livro} = await emprestimoRepository.buscarEmprestimoPorId(id);
+    await emailService.enviar({
+      data_saida, 
+      data_retorno, 
+      nome_usuario: usuario.nome, 
+      email: usuario.email, 
+      titulo_livro: livro.titulo
+    });
+
     return Either.right(null);
   };
 }
--- src/application/emprestar-livro.usecase.spec.js
+++ src/application/emprestar-livro.usecase.spec.js
@@ -6,7 +6,12 @@ describe('Emprestar Livro UseCase', function() {
     existeLivroPendenteUsuario: jest.fn(),
     quantidadeLivrosEmprestadoPorUsuario: jest.fn(),
     emprestarLivro: jest.fn(),
+    buscarEmprestimoPorId: jest.fn(),
   };
+
+  const emailService = {
+    enviar: jest.fn(),
+  }
 
   const emprestarLivroPayload = {
     livro_id: 'id_valido',
@@ -20,16 +25,35 @@ describe('Emprestar Livro UseCase', function() {
   });
 
   test('Deve emprestar um livro', async () => {
-    const sut = emprestarLivroUsecase({ emprestimoRepository });
+    emprestimoRepository.emprestarLivro.mockResolvedValue('id_valido');
+    emprestimoRepository.buscarEmprestimoPorId.mockResolvedValue({
+      usuario: {
+        nome: 'usuario',
+        email: 'email',
+      },
+      livro: {
+        titulo: 'titulo',
+      }
+    })
+    const sut = emprestarLivroUsecase({ emprestimoRepository, emailService });
     const output = await sut(emprestarLivroPayload);
 
     expect(output.right).toBeNull();
     expect(emprestimoRepository.emprestarLivro).toHaveBeenCalledWith(emprestarLivroPayload);
-    expect(emprestimoRepository.emprestarLivro).toHaveBeenCalledTimes(1);   
+    expect(emprestimoRepository.emprestarLivro).toHaveBeenCalledTimes(1);
+    expect(emailService.enviar).toHaveBeenCalledWith({
+      data_saida: emprestarLivroPayload.data_saida,
+      data_retorno: emprestarLivroPayload.data_retorno,
+      nome_usuario: 'usuario',
+      email: 'email',
+      titulo_livro: 'titulo',
+    });
+    expect(emailService.enviar).toHaveBeenCalledTimes(1);   
   });
 
   test('deve retornar error se a data de saída for maior que a data de retorno', async () => {
-    const sut = emprestarLivroUsecase({ emprestimoRepository });
+    const sut = emprestarLivroUsecase({ emprestimoRepository, emailService });
+
 
     const output = await sut({
       ...emprestarLivroPayload,
@@ -41,7 +65,8 @@ describe('Emprestar Livro UseCase', function() {
   })
 
   test('não deve permitir que um livro seja emprestado para um usuário que já possui o livro', async () => {
-    const sut = emprestarLivroUsecase({ emprestimoRepository });
+        const sut = emprestarLivroUsecase({ emprestimoRepository, emailService });
+
     emprestimoRepository.existeLivroPendenteUsuario.mockResolvedValue(true);
 
     const output = await sut(emprestarLivroPayload);
@@ -50,7 +75,8 @@ describe('Emprestar Livro UseCase', function() {
   });
 
   test('não deve permitir que um usuário pegue emprestado mais de 3 livros', async () => {
-    const sut = emprestarLivroUsecase({ emprestimoRepository });
+        const sut = emprestarLivroUsecase({ emprestimoRepository, emailService });
+
     emprestimoRepository.existeLivroPendenteUsuario.mockResolvedValue(false);
     emprestimoRepository.quantidadeLivrosEmprestadoPorUsuario.mockResolvedValue(3);
 
@@ -64,9 +90,10 @@ describe('Emprestar Livro UseCase', function() {
   });
 
   test('deve retornar erro se os campos obrigatórios não forem informados', async () => {
-    const sut = emprestarLivroUsecase({ emprestimoRepository });
+        const sut = emprestarLivroUsecase({ emprestimoRepository, emailService });
+
     const output = await sut({});
     expect(output).toEqual(Either.left(AppError.fieldsObligatory));
   });
-
+ 
 });
