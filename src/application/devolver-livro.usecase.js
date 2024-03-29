5d05f555fe6ecfae5816786b8385cb95008bd072
leo <nascimentoleo899@gmail.com>
2024-03-29 17:45:15 -0300

    Add multa calculation in devolverLivroUseCase

--- src/application/devolver-livro.usecase.js
+++ src/application/devolver-livro.usecase.js
@@ -2,11 +2,12 @@ const { Either } = require("../shared/errors");
 
 module.exports = function devolverLivroUseCase({ emprestimoRepository}) {
   return async function ({ emprestimo_id, data_devolucao }) {
-    await emprestimoRepository.devolverLivro({ emprestimo_id, data_devolucao });
-
+    const {data_retorno} = await emprestimoRepository.devolverLivro({ emprestimo_id, data_devolucao });
+    const devolucaoNoPrazo = new Date(data_devolucao).getTime() <= new Date(data_retorno).getTime();
+    const multa = devolucaoNoPrazo ? 0.0 : 10.0;
     return Either.right({
       emprestimo_id,
-      multa: 0.0,
+      multa,
     });
   }
 }
--- src/application/devolver-livro.usecase.spec.js
+++ src/application/devolver-livro.usecase.spec.js
@@ -6,6 +6,9 @@ describe('Devolver Livro UseCase', function() {
     devolverLivro: jest.fn(),
   };
   test('Deve devolver um livro sem multa', async () => {
+    emprestimoRepository.devolverLivro.mockResolvedValue({
+      data_retorno: new Date('2024-02-23'),
+    });
     const payload = {
       emprestimo_id: 'id_valido',
       data_devolucao: new Date('2024-02-23'),
@@ -20,6 +23,24 @@ describe('Devolver Livro UseCase', function() {
     })
     expect(emprestimoRepository.devolverLivro).toHaveBeenCalledWith(payload);
     expect(emprestimoRepository.devolverLivro).toHaveBeenCalledTimes(1);
+  });
+
+  test('deve devolver um livro com multa', async () => {
+    emprestimoRepository.devolverLivro.mockResolvedValue({
+      data_retorno: new Date('2024-02-23'),
+    });
+    const payload = {
+      emprestimo_id: 'id_valido',
+      data_devolucao: new Date('2024-02-24'),
+    };
+
+    const sut = devolverLivroUsecase({ emprestimoRepository });
+    const output = await sut(payload);
+
+    expect(output.right).toEqual({
+      emprestimo_id: 'id_valido',
+      multa: 10.0,
+    })
   });
 
 })
