eb05f480dffa87f053e40e1d049b812d6927bbc0
leo <nascimentoleo899@gmail.com>
2024-03-30 21:14:22 -0300

    Add error handling and dependency check in cadastrarUsuarioController

--- src/interface-adpter/controller/cadastrar-usuario.controller.js
+++ src/interface-adpter/controller/cadastrar-usuario.controller.js
@@ -1,12 +1,14 @@
 const httpResponse = require("../../helpers/http.response");
+const AppError = require("../../shared/errors/AppError");
 
 module.exports = async function cadastrarUsuarioController({
   cadastraUsuarioUseCase, httpRequest
 }) {
+  if(!cadastraUsuarioUseCase || !httpRequest) throw new AppError(AppError.dependencies);
   const { nome, cpf, endereco, telefone, email } = httpRequest.body;
   const output = await cadastraUsuarioUseCase({ nome, cpf, endereco, telefone, email});
   return output.fold(
-    (error) => httpResponse(400, error.message),
+    (error) => httpResponse(400, error),
     () => httpResponse(201, null)
   )
 }
--- src/interface-adpter/controller/cadastrar-usuario.controller.spec.js
+++ src/interface-adpter/controller/cadastrar-usuario.controller.spec.js
@@ -1,3 +1,5 @@
+const httpResponse = require("../../helpers/http.response");
+const AppError = require("../../shared/errors/AppError");
 const Either = require("../../shared/errors/Either");
 const cadastrarUsuarioController = require("./cadastrar-usuario.controller");
 
@@ -23,4 +25,24 @@ describe('Cadastrar Usuario Controller', function() {
     expect(cadastraUsuarioUseCase).toHaveBeenCalledTimes(1);
   });
 
+  test('deve retornar exception se dependencias nao forem passadas', async function(){
+    await expect(() => cadastrarUsuarioController({})).rejects.toThrow(new AppError(AppError.dependencies));
+  });
+
+  test('deve retornar 400 se houver erro ao cadastrar usuario', async function(){
+    cadastraUsuarioUseCase.mockResolvedValueOnce(Either.left({message: 'mensagem de erro'}));
+    const httpRequest = {
+      body: {
+        nome: 'nome',
+        cpf: 'cpf',
+        endereco: 'endereco',
+        telefone: 'telefone',
+        email: 'email',
+      }
+    }
+
+    const sut = await cadastrarUsuarioController({cadastraUsuarioUseCase, httpRequest});
+    
+    expect(sut).toEqual(httpResponse(400, {message: 'mensagem de erro'}));
+  });
 });
