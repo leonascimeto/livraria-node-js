17b60b57f359f6ad3e57672b94893fde16189864
leo <nascimentoleo899@gmail.com>
2024-03-30 22:00:29 -0300

    Add buscarUsuarioPorCpfController and its test

---
+++ src/interface-adpter/controller/buscar-usuario-por-cpf.controller.js
@@ -0,0 +1,19 @@
+const { z } = require("zod");
+const httpResponse = require("../../helpers/http.response");
+const { AppError } = require("../../shared/errors");
+
+const zodSchema = z.object({
+  cpf: z
+    .string({required_error: "CPF é obrigatório"})
+    .refine(value => /^([0-9]{3}\.){2}[0-9]{3}-[0-9]{2}$/.test(value)),
+});
+
+module.exports = async function buscarUsuarioPorCpfController({ buscarUsuarioPorCpfUseCase, httpRequest }) {
+    if(!buscarUsuarioPorCpfUseCase || !httpRequest) throw new AppError(AppError.dependencies);
+    const { cpf } = zodSchema.parse(httpRequest.params);
+    const output = await buscarUsuarioPorCpfUseCase({cpf});
+    return output.fold(
+      (error) => httpResponse(400, error.message),
+      (usuario) => httpResponse(200, usuario)
+    );
+}
---
+++ src/interface-adpter/controller/buscar-usuario-por-cpf.controller.spec.js
@@ -0,0 +1,68 @@
+const { ZodError } = require("zod");
+const httpResponse = require("../../helpers/http.response");
+const { Either, AppError } = require("../../shared/errors");
+const buscarUsuarioPorCpfController = require("./buscar-usuario-por-cpf.controller");
+
+describe('Buscar Usuario Por CPF Controller', function() {
+  const buscarUsuarioPorCpfUseCase = jest.fn();
+
+  const usuarioMock = {
+    id: 'id_valido',
+    name: 'Carlos Silva',
+    cpf: '111.111.111-11',
+    telefone: '(11) 99999-9999',
+    email: 'email@email.com',
+    endereco: 'Rua dos Alfeneiros, 4'
+  };
+  
+  test('deve retornar usuario com status 200 quando o usuario for encontrado com sucesso', async function(){
+    buscarUsuarioPorCpfUseCase.mockResolvedValueOnce(Either.right(usuarioMock));
+    const httpRequest = {
+      params: {
+        cpf: '111.111.111-11',
+      }
+    };
+
+    const sut = await buscarUsuarioPorCpfController({buscarUsuarioPorCpfUseCase, httpRequest});
+
+    expect(sut).toEqual(httpResponse(200, usuarioMock));
+    expect(buscarUsuarioPorCpfUseCase).toHaveBeenCalledWith(httpRequest.params);
+    expect(buscarUsuarioPorCpfUseCase).toHaveBeenCalledTimes(1);
+  });
+
+  test('deve retornar exception se dependencias nao forem passadas', async function(){
+    await expect(() => buscarUsuarioPorCpfController({})).rejects.toThrow(new AppError(AppError.dependencies));
+  });
+
+  test('deve retornar 200 e null se usuario não for encontrado', async function(){
+    buscarUsuarioPorCpfUseCase.mockResolvedValueOnce(Either.right(null));
+    const httpRequest = {
+      params: {
+        cpf: '111.111.111-11',
+      }
+    };
+
+    const sut = await buscarUsuarioPorCpfController({buscarUsuarioPorCpfUseCase, httpRequest});
+
+    expect(sut).toEqual(httpResponse(200, null));
+  });
+
+  test('deve retornar exception caso passe um cpf inválido', async function(){
+    const httpRequest = {
+      params: {
+        cpf: '111.111.111-1',
+      }
+    };
+
+    await expect(() => buscarUsuarioPorCpfController({buscarUsuarioPorCpfUseCase, httpRequest})).rejects.toBeInstanceOf(ZodError);
+  });
+
+  test('deve retornar exception caso o cpf não seja informado', async function(){
+    const httpRequest = {
+      params: {}
+    };
+
+    await expect(() => buscarUsuarioPorCpfController({buscarUsuarioPorCpfUseCase, httpRequest})).rejects.toBeInstanceOf(ZodError);
+  });
+
+});
