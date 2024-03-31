694b8dc994271695f98d05cc07dff76c82aa71a9
leo <nascimentoleo899@gmail.com>
2024-03-31 12:16:02 -0300

    Add devolver-livro.controller.js and devolver-livro.controller.spec.js

---
+++ src/interface-adpter/controller/devolver-livro.controller.js
@@ -0,0 +1,25 @@
+const httpResponse = require("../../helpers/http.response");
+const { AppError } = require("../../shared/errors");
+const z = require("zod");
+
+const zodSchemaBody = z.object({
+  data_devolucao: z.string({required_error: "Data de devolução é obrigatória"}),
+});
+
+const zodSchemaParams = z.object({
+  emprestimo_id: z.number({required_error: "ID do empréstimo é obrigatório"}),
+});
+
+module.exports = async function devolverLivroController({ devolverLivroUseCase, httpRequest }) {
+  if(!devolverLivroUseCase || !httpRequest) throw new AppError(AppError.dependencies);
+  const {  data_devolucao } = zodSchemaBody.parse(httpRequest.body);
+  const { emprestimo_id } = zodSchemaParams.parse(httpRequest.params);
+  const output = await devolverLivroUseCase({ 
+    emprestimo_id, 
+    data_devolucao: new Date(data_devolucao) 
+  });
+  return output.fold(
+    (error) => httpResponse(400, error.message),
+    (resultado) => httpResponse(200, resultado)
+  );
+}
---
+++ src/interface-adpter/controller/devolver-livro.controller.spec.js
@@ -0,0 +1,56 @@
+const { ZodError } = require("zod");
+const httpResponse = require("../../helpers/http.response");
+const { Either, AppError } = require("../../shared/errors");
+const devolverLivroController = require("./devolver-livro.controller");
+
+describe('Devolver Livro Controller', () => {
+  const devolverLivroUseCase = jest.fn();
+  test('deve devolver livro e retornar statusCode 200', async () => {
+    devolverLivroUseCase.mockResolvedValue(Either.right({
+      emprestimo_id: 1,
+      multa: 0,
+    }));
+    const httpRequest = {
+      body: {
+        data_devolucao: '2024-02-16',
+      },
+      params: {
+        emprestimo_id: 1,
+      },
+    };
+
+    const sut = await devolverLivroController({ devolverLivroUseCase, httpRequest });
+
+    expect(sut).toEqual(httpResponse(200, { emprestimo_id: 1, multa: 0 }));
+    expect(devolverLivroUseCase).toHaveBeenCalledTimes(1);
+  });
+
+  test('deve retornar 400 se o livro não for devolvido com sucesso', async () => {
+    devolverLivroUseCase.mockResolvedValue(Either.left({ message: 'erro' }));
+    const httpRequest = {
+      body: {
+        data_devolucao: '2024-02-16',
+      },
+      params: {
+        emprestimo_id: 1,
+      },
+    };
+
+    const sut = devolverLivroController({ devolverLivroUseCase, httpRequest });
+    const output = await sut;
+
+    expect(output).toEqual(httpResponse(400, 'erro'));
+  });
+
+  test('deve retornar exception se dependencias nao forem passadas', async () => {
+    await expect(() => devolverLivroController({})).rejects.toThrow(new AppError(AppError.dependencies));
+  });
+
+  test('deve retornar um erro do ZOD caso dados sejam inválidos', function(){
+    const httpRequest = {
+      body: {},
+      params: {},
+    }
+    expect(() => devolverLivroController({devolverLivroUseCase, httpRequest})).rejects.toBeInstanceOf(ZodError);
+  });
+});
