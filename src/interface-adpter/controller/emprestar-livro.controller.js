b5da44358b53efbb8c3560d450551c02b854e1e2
leo <nascimentoleo899@gmail.com>
2024-03-31 11:55:16 -0300

    Add emprestar-livro.controller.js and emprestar-livro.controller.spec.js

---
+++ src/interface-adpter/controller/emprestar-livro.controller.js
@@ -0,0 +1,26 @@
+const httpResponse = require("../../helpers/http.response");
+const { AppError } = require("../../shared/errors");
+
+const { z } = require("zod");
+
+const zodSchema = z.object({
+  livro_id: z.number({required_error: "ID do livro é obrigatório"}),
+  usuario_id: z.number({required_error: "ID do usuário é obrigatório"}),
+  data_saida: z.string({required_error: "Data de saída é obrigatória"}),
+  data_retorno: z.string({required_error: "Data de retorno é obrigatória"})
+});
+
+module.exports = async function emprestarLivroController({ emprestarLivroUseCase, httpRequest }) {
+  if(!emprestarLivroUseCase || !httpRequest) throw new AppError(AppError.dependencies);
+  const { livro_id, usuario_id, data_saida, data_retorno } = zodSchema.parse(httpRequest.body);
+  const output = await emprestarLivroUseCase({ 
+    livro_id, 
+    usuario_id, 
+    data_saida: new Date(data_saida), 
+    data_retorno: new Date(data_retorno) 
+  });
+  return output.fold(
+    (error) => httpResponse(400, error.message),
+    () => httpResponse(200, null)
+  );
+}
---
+++ src/interface-adpter/controller/emprestar-livro.controller.spec.js
@@ -0,0 +1,55 @@
+const httpResponse = require("../../helpers/http.response");
+const { Either, AppError } = require("../../shared/errors");
+const emprestarLivroController = require("./emprestar-livro.controller");
+
+describe('Emprestar Livro Controller', function() {
+  const emprestarLivroUseCase = jest.fn();
+
+  const emprestarLivroPayload = {
+    livro_id: 1,
+    usuario_id: 1,
+    data_saida: '2024-02-16',
+    data_retorno: '2024-02-23',
+  };
+
+  test('deve emprestar um livro', async function(){
+    emprestarLivroUseCase.mockResolvedValue(Either.right(null));
+    const httpRequest = {
+      body: emprestarLivroPayload
+    }
+
+    const sut = await emprestarLivroController({ emprestarLivroUseCase, httpRequest });
+
+    expect(sut).toEqual(httpResponse(200, null));
+    expect(emprestarLivroUseCase).toHaveBeenCalledWith({
+      ...emprestarLivroPayload,
+      data_saida: new Date(emprestarLivroPayload.data_saida),
+      data_retorno: new Date(emprestarLivroPayload.data_retorno),
+    });
+    expect(emprestarLivroUseCase).toHaveBeenCalledTimes(1);
+  });
+
+  test('deve retornar 400 se o livro não for emprestado com sucesso', async function(){
+    emprestarLivroUseCase.mockResolvedValue(Either.left({ message: 'erro' }));
+    const httpRequest = {
+      body: emprestarLivroPayload
+    }
+
+    const sut = emprestarLivroController({ emprestarLivroUseCase, httpRequest });
+    const output = await sut;
+
+    expect(output).toEqual(httpResponse(400, 'erro'));
+  });
+
+  test('deve retornar exception se dependencias nao forem passadas', async function(){
+    await expect(() => emprestarLivroController({})).rejects.toThrow(new AppError(AppError.dependencies));
+  });
+
+  test('deve retornar exception do zodValidation caso os dados estejam invalidos', async function(){
+    const httpRequest = {
+      body: {}
+    };
+
+    await expect(() => emprestarLivroController({ emprestarLivroUseCase, httpRequest })).rejects.toBeInstanceOf(Error);
+  });
+});
