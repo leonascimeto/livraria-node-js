d99bd9e044bed1360342c394bd384d67a99a236d
leo <nascimentoleo899@gmail.com>
2024-04-02 20:10:57 -0300

    Add livro routes to API

--- src/infra/http/api/routes/index.js
+++ src/infra/http/api/routes/index.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const { usuariosRoutes } = require('./usuario.routes');
+const { livroRoutes } = require('./livro.routes');
 
 const routes = Router();
 routes.use('/usuarios', usuariosRoutes);
+routes.use('/livros', livroRoutes);
 
 module.exports = routes;
---
+++ src/infra/http/api/routes/livro.routes.js
@@ -0,0 +1,5 @@
+const { Router } = require('express');
+
+const livroRoutes = Router();
+
+module.exports = { livroRoutes }
